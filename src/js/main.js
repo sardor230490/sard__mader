0906a113b070348748059803c6eef6a0d4c38069
sardor230490 <sardor.ibragimov.1990@gmail.com>
2020-10-11 19:10:30 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,63 @@
+$(function () {
+	$(".rate-star").rateYo({
+		rating: 5, 
+		starWidth: "12px",
+    readOnly: true
+
+	});
+
+	$(".product__slider-inner").slick({
+		dots:true,
+		arrows:false, 
+		slidesToShow: 4,
+		slidesToScroll: 4,
+
+	}); 
+	
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+	 
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+});
+
+
+$(".icon-th-list").on("click", function() {
+	$(".product__item").addClass("list");
+	$(this).addClass("active"); 
+	$(".icon-th-large").removeClass("active"); 
+ 
+}); 
+$(".icon-th-large").on("click", function() {
+	$(".product__item").removeClass("list");
+	$(this).addClass("active"); 
+	$(".icon-th-list").removeClass("active"); 
+ 
+}); 
+
+$(".menu__btn").on('click', function(){
+	$(".menu-list").slideToggle();
+	
+
+});
+$(".header__btn-menu").on('click', function(){
+	$(".header__box").toggleClass("active");
+
+	
+
+});
+$('.product-one__tabs .tab, .settings__tabs .tabs').on('click', function(event) {
+	var id = $(this).attr('data-id');
+		$('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#'+id).addClass('active-tab').fadeIn();
+		return false;
+	});
+
+	var mixer = mixitup('.product__inner-box');
+
+});
